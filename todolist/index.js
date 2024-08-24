2c690bd922c7e714742d81c77d3fc83f8f781e0f
singhcodez <jasnoor155@gmail.com>
2024-08-24 07:35:46 +0530

    update index.js

--- todolist/index.js
+++ todolist/index.js
@@ -1,7 +1,7 @@
 let id=0;
 let tasks=[]
 const task=document.getElementById("task");
-//task adding in list & in task array
+//tasks adding in in task array
   document.getElementById("add").addEventListener("click",(e)=>{
     e.preventDefault();
     
@@ -11,15 +11,14 @@ const task=document.getElementById("task");
    }
     tasks=[newTask,...tasks]
     task.value="";
-    console.log(tasks)
     
-      // adding task 
-  let list = document.getElementById("list");
-  let div=document.createElement("div")
-  
-      tasks.map((task,i)=>{ let item=` 
+      // adding task in list
+    const addTasks=(div)=>{
+      let list = document.getElementById("list");
+        tasks.map((task,i)=>{ 
+      let item=` 
              <li class="item">
-              <p class="text"><b>${i+1} </b>${task.text}</p> 
+              <p class="text">${task.text}</p> 
               <div class="editSec">
                 <span class="edit">✍🏻</span>
                 <span class="delete">❌</span>
@@ -30,12 +29,42 @@ const task=document.getElementById("task");
       div.innerHTML+=item;
   })
    list.replaceChild(div,list.childNodes[0]);   
+   
+   }
+   let div=document.createElement("div")
+   addTasks(div);
+      
+     
+    // edit
     
-  })
+})
   
     // delete elements 
     
-
-
+    const deleteTask=()=>{
+   let item=document.querySelectorAll(".item");
+   let deletebtn=document.querySelectorAll(".delete");
+       
+      
+      deletebtn.forEach((tapToDel,j)=>{
+       //console.log("outer",j)
+      
+        deletebtn[j].addEventListener("click",()=>{
+          console.log(  item.length,tasks.length ,item.length==tasks.length,j )
           
- console.log(tasks)
+          if(item.length==tasks.length && j<item.length){
+      //    console.log("inner",j)
+           item[j].remove();
+          let deletedItem=tasks.splice(j,1);
+          let div=document.createElement("div");
+          deletebtn=document.querySelectorAll(".delete");
+         
+         }
+         
+        })
+    
+   })
+    
+    }
+   document.body.addEventListener("click",deleteTask) 
+      
